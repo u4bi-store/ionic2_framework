3d5c50386be8e206268cd58fc605ae6eba23b863
u4bi <u4bi@u4bi.com>
2016-12-15 15:38:56 +0000

    pages users.ts - json api render console test

--- indexApp/src/models/user.ts
+++ indexApp/src/models/user.ts
@@ -1,7 +1,7 @@
 
 export interface User{
   login: string;
-  avatar_url: atring;
+  avatar_url: string;
   public_repos: number;
   public_gists: number;
   followers: number;
--- indexApp/src/pages/users/users.ts
+++ indexApp/src/pages/users/users.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-/*
-  Generated class for the Users page.
+import { User } from '../../models/user'; /* 모델 임포트*/
+import { GithubUsers } from '../../providers/github-users'; /* 정의된 프로바이더 임포트 */
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-users',
   templateUrl: 'users.html'
 })
 export class UsersPage {
+  users: User[] /* 배열 정의*/
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, private githubUsers: GithubUsers) {
+    githubUsers.load().subscribe(users => { /* 로드해서 반환되는 값을 users에 주입*/
+      console.log(users); /* users 어레이 콘솔 테스트*/
+      /* @ 결과 Array[30] 개 넘어옴 */
+    })
+  }
 
   ionViewDidLoad() {
     console.log('Hello UsersPage Page');
