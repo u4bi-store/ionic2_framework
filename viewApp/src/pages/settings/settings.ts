15fb87040c48e689897fd148c5a1ca2c0f94816c
u4bi <u4bi@u4bi.com>
2016-12-17 09:52:36 +0000

    view app - settings view init

--- viewApp/src/pages/settings/settings.ts
+++ viewApp/src/pages/settings/settings.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+
+import {RedditService} from '../../app/services/reddit.service';
+import {DetailsPage} from '../details/details';
 
 @Component({
   selector: 'settings',
   templateUrl: 'settings.html'
 })
 export class SettingsPage {
+  category: any;
+  limit: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private redditService:RedditService) {
+    this.getDefaults();
+  }
 
+  getDefaults(){
+    this.category = 'sports';
+    this.limit = 10;
   }
 
 }
