d5799d0008aa7764c997d2675e0f17f01d4bfe95
u4bi <u4bi@u4bi.com>
2016-12-17 17:22:57 +0000

    love calcullator - get score() logic comment

--- love-calculator/src/pages/home/home.ts
+++ love-calculator/src/pages/home/home.ts
@@ -10,7 +10,12 @@ export class HomePage { /* 홈페이지 클래스*/
   name1 = ''; /* name 1, 2 초기화*/
   name2 = '';
   
-  get score() {
-    return 77;
+  get score() { /* 리턴*/
+    const letters = (this.name1 + this.name2); /* 두 값을 더해 초기화*/
+    let sum = 0; /* sum 정의 */
+    for(let i = 0; i < letters.length; i++){ /* 랭쓰까지 포문을 돌리고*/
+      sum += letters.charCodeAt(i); /* 더해줌*/
+    }
+    return sum % 101; /* 리턴 %101*/
   }
 }
