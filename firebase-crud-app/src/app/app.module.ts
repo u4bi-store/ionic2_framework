d1de1133733fa6e0dc60dba919fc81bab37c344b
u4bi <u4bi@u4bi.com>
2016-12-18 11:58:41 +0000

    firebase-crud-app - app.module firebase config module import

--- firebase-crud-app/src/app/app.module.ts
+++ firebase-crud-app/src/app/app.module.ts
@@ -3,13 +3,24 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2'; /* 앵귤러파이어모듈 임포트*/
+  
+export const firebaseConfig = { /* 파이어베이스 셋업*/
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig) /* 정의된 셋업대로 모듈에 추가함*/
   ],
   bootstrap: [IonicApp],
   entryComponents: [
