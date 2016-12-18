e6bbbb3fea44b8447b1ac5dc116307054080740e
u4bi <u4bi@u4bi.com>
2016-12-18 04:03:51 +0000

    socket.io-chat-app - socket import

--- socket.io-chat-app/src/pages/home/home.ts
+++ socket.io-chat-app/src/pages/home/home.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { Component, NgZone, ViewChild } from '@angular/core';
+import * as io from 'socket-io-client';
+import { NavController, Content } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild(Content) content: Content;
+  messages:any = [];
+  socketHost: string = "http://localhost:3000/";
+  socket:any;
+  chat:any;
+  username:string;
+  zone:any;
+  
   constructor(public navCtrl: NavController) {
-    
+
   }
 
 }
