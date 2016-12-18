0cc053d1e8b54eb88afe22e9a82c760892283de3
u4bi <u4bi@u4bi.com>
2016-12-18 13:55:02 +0000

    firebase-crud-app - firebase showOptions delete, update init

--- firebase-crud-app/src/pages/home/home.ts
+++ firebase-crud-app/src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
-import { NavController, AlertController } from 'ionic-angular'; /* 얼럿 컨트롤러 임포트*/
+import { NavController,
+         AlertController, /* 얼럿 컨트롤러 임포트*/
+         ActionSheetController } from 'ionic-angular'; /* 액션시트 임포트*/
 
 import {AngularFire, FirebaseListObservable} from 'angularfire2'; /* 파이어베이스 임포트*/
 
@@ -12,7 +14,7 @@ export class HomePage {
   
   songs: FirebaseListObservable<any>; /* songs  변수만듬 */
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire) { /* 생성자에 파이어베이스 넘김*/
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire, public actionSheetCtrl: ActionSheetController) { /* 생성자에 파이어베이스 넘김*/
     this.songs = af.database.list('/songs'); /*파이어 베이스에 주입 후에 songs를 초기화함*/
   }
 
@@ -47,5 +49,33 @@ export class HomePage {
     
     prompt.present(); /* 위에 객체로 정의된거 표출함*/
   }
+
+  showOptions(songId, songTitle) { /* 노래번호, 노래제목*/
+    let actionSheet = this.actionSheetCtrl.create({ /* 액션 시트 생성*/
+      title: ' 노래를 삭제하거나 제목을 수정할 수 있습니다.',
+      buttons: [
+        {
+          text: '노래삭제',
+          role: 'destructive',
+          handler: () => {
+            //this.removeSong(songId); /* 인자로 받은 노래번호 삭제함*/
+          }
+        },{
+          text: '제목수정',
+          handler: () => {
+            //this.updateSong(songId, songTitle);  /* 노래번호에 대입되는 제목을 수정함*/
+          }
+        },{
+          text: '취소',
+          role: 'cancel',
+          handler: () => {
+            console.log('클릭 취소함');
+          }
+        }
+      ]
+    });
     
+    actionSheet.present(); /* 위에 객체로 정의된거 표출함*/
+  }
+
 }
