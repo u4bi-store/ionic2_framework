601ad452640fd7eed8086bd144f3ecf972c90ed6
u4bi <u4bi@u4bi.com>
2017-02-20 15:06:11 +0900

    github service render - gitProfile - gitRepos

--- detail-github/src/providers/github-service.ts
+++ detail-github/src/providers/github-service.ts
@@ -10,9 +10,20 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class GithubService {
+  baseUrl: String;
 
   constructor(public http: Http) {
-    console.log('Hello GithubService Provider');
+    this.baseUrl = 'https://api.github.com/users/';
+  }
+
+  getProfile(username){
+    return this.http.get(this.baseUrl + username)
+      .map(res => res.json());
+  }
+
+  getRepos(username){
+    return this.http.get(this.baseUrl + username + '/repos')
+      .map(res => res.json());
   }
 
 }
