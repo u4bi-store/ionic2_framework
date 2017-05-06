09eb2b18cb7eec4fae6087a85d2e445e06e18023
u4bi <u4bi@u4bi.com>
2017-05-07 00:50:05 +0900

    alertController render

--- layout/src/pages/alert/alert.ts
+++ layout/src/pages/alert/alert.ts
@@ -56,4 +56,41 @@ export class Alert {
     alert.present();
   }
 
+  basicPrompt(){
+    let alert = this.alertCtrl.create({
+      title : '아이디 입력',
+      inputs:[
+        {
+          label : '아이디',
+          name : 'id',
+          placeholder : '아이디 입력해주세요'
+        },
+        {
+          label : '비밀번호',
+          name : 'pass',
+          placeholder : '비밀번호 입력해주세요.',
+          type : 'password'
+        }
+      ],
+      buttons : [
+        {
+          text : '취소',
+          handler : data =>{
+            console.log('취소버튼 누름');
+          }
+        },
+        {
+          text : '로그인',
+          handler : data =>{
+            console.log('로그인 버튼 누름');
+            console.log('아이디 : ', data.id);
+            console.log('비밀번호 : ', data.pass);
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
 }
